ab3e535527d0b5ddc807a207e95399310951ec19
Tyler Peters <tylerjpeters@users.noreply.github.com>
2017-12-03 14:40:17 -0600

    Completed the project and re-tested with the sample input files.

--- src/IndexThread.java
+++ src/IndexThread.java
@@ -1,17 +1,90 @@
+/*
+    Project 5 (Mulithreading in Java)
+    Tyler Peters
+    2017-12-1
+    DS 730
+
+    Each text files found at the input location is processed by
+    this class (which is called from IndexThreads.java).
+ */
+
 import java.io.*;
+import java.util.*;
 
 public class IndexThread extends Thread {
 
     private File file;
     private int numChars;
 
-    public IndexThread (File file, int numChars) {
+    public IndexThread(File file, int numChars) {
         this.file = file;
         this.numChars = numChars;
     }
 
     public void run() {
+        try {
+            // Instantiate a new data structure within which to store and order the index items:
+            TreeMap<String, TreeSet<Integer>> index = new TreeMap<>();
 
+            int charCount = 0;
+            int page = 1;
+            index.clear();
+            Scanner reader = new Scanner(new BufferedReader(new FileReader(file)));
+            // Loop through the content within the file:
+            while (reader.hasNext()) {
+                String str = reader.next();
+                if (charCount + str.length() <= numChars) {
+                    charCount += str.length();
+                    updateIndex(index, str, page);
+                } else {
+                    page++;
+                    charCount = str.length();
+                    updateIndex(index, str, page);
+                }
+            }
+            // For each entry in the TreeMap (index), build a string in the specified format:
+            StringBuilder sb = new StringBuilder();
+            for (Map.Entry<String, TreeSet<Integer>> entry : index.entrySet()) {
+                StringBuilder pages = new StringBuilder();
+                sb.append(entry.getKey());
+                if (entry.getValue() != null) {
+                    for (Integer value : entry.getValue()) {
+                        pages.append(", ").append(value.toString());
+                    }
+                    sb.append(pages.substring(1, pages.length()));
+                }
+                sb.append("\n");
+            }
+            // Build the output filename:
+            String fileName = file.getParent() + "/output/" + file.getName().replace(".txt", "") + "_output.txt";
+            // Select the path to the output directory
+            File output = new File(new File(fileName).getParent());
+            // If the output directory does not exist, make it:
+            if (!output.exists() || !output.isDirectory()) {
+                output.mkdirs();
+            }
+
+            // Open a buffered file writer at the output location:
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            // Write the index to the file:
+            writer.write(sb.toString());
+            // Close the writer:
+            writer.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // This method adds an entry to the index if it does not exist; otherwise it appends the page number
+    // to the existing entry:
+    private static void updateIndex(TreeMap<String, TreeSet<Integer>> index, String str, int page) {
+        if (index.containsKey(str.toLowerCase())) {
+            index.get(str.toLowerCase()).add(page);
+        } else {
+            TreeSet<Integer> set = new TreeSet<>();
+            set.add(page);
+            index.put(str.toLowerCase(), set);
+        }
     }
 
 }
--- src/IndexThreads.java
+++ src/IndexThreads.java
@@ -6,16 +6,43 @@
 
     Multi-threaded program that accepts the location of an
     input directory followed by the number of characters per page.
+
+    Each text files found at the input location will be processed on
+    a dedicated thread (defined within IndexThread.java).
  */
 
+import java.io.*;
+
 public class IndexThreads {
-    public static void main (String[] args) {
+    public static void main (String[] args) throws Exception {
+
         // Collect the start time of the program:
         final long startTime = System.currentTimeMillis();
+
         // Parse and assign the input variables:
         String location = args[0];
         int numCharacters = Integer.parseInt(args[1]);
-        // Load the input file paths into a list:
 
+        // Load the input file paths into a list:
+        File path = new File(location);
+        File[] inputFiles = path.listFiles((File filename) -> filename.getName().endsWith(".txt"));
+        // Create an array of uninstantiated IndexThread objects (one for each file):
+        IndexThread[] indices = new IndexThread[inputFiles.length];
+
+        // Instantiate each of the IndexThread objects with the file name and character count:
+        for (int i = 0; i < inputFiles.length; i++) {
+            indices[i] = new IndexThread(inputFiles[i], numCharacters);
+            indices[i].start();
+        }
+
+        // Test and join the threads:
+        for (IndexThread index : indices) {
+            if (index.isAlive()) {
+                index.join();
+            }
+        }
+
+        // Print the runtime of the program to the console window (in milliseconds):
+        System.out.println(System.currentTimeMillis() - startTime);
     }
 }
